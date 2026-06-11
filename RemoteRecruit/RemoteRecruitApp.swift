dd3ca0f6f8a3b957d2d70443ab4b0c9153136f8b
Vijaya-venkatesh <vijayagovindaraj@bitcot.com>
2026-06-11 16:31:14 +0530

    Initial Commit

---
+++ RemoteRecruit/RemoteRecruitApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteRecruitApp.swift
+//  RemoteRecruit
+//
+//  Created by bitcot on 11/06/26.
+//
+
+import SwiftUI
+
+@main
+struct RemoteRecruitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
